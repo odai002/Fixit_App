27209dca326741c064fd0a648432c1f69f821561
Mojo <mhamad.alshame2134@gmail.com>
2024-11-27 19:35:58 +0400

    kan-88
    fixing home page design

--- lib/View/Screen/Home_Owner/Input_onwer/Home_app/Category_Page/Category_Page_Components/Custom_Card_Widget.dart
+++ lib/View/Screen/Home_Owner/Input_onwer/Home_app/Category_Page/Category_Page_Components/Custom_Card_Widget.dart
@@ -1,18 +1,7 @@
-import 'dart:ui';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
-
-import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-
+import '../../../../../../Widgets/Custom_Button.dart';
 import '../../../../../../Widgets/Custom_Star_widget.dart';
-import '../../../../../Contractor/Input_contractor/profile_page.dart';
-import '../../detail_page.dart';
-import '../../task_page.dart';
-
 
 class CustomCard extends StatelessWidget {
   final Image image;
@@ -22,200 +11,243 @@ class CustomCard extends StatelessWidget {
   final String number;
   final String category;
   final String local;
-  final double radius;
+  final String buttonText;
+  final Function(int) onPressed;
+  final int id;
+
 
   const CustomCard({
-    Key? key,
+    super.key,
     required this.image,
-    required this.name,
     required this.email,
     required this.desc,
     required this.number,
+    required this.name,
     required this.category,
     required this.local,
-    this.radius = 55,
+    required this.buttonText,
+    required this.onPressed, required this.id,
 
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => ContractorProfilePage()));
-
-      },
-      child: Card(
-        color: Colors.white,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-        elevation: 6,
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Top Row with Profile Picture, Name, Category, and Star Rating
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Material(
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(radius)),
-
-                    elevation: 8,
-                    child: CircleAvatar(
-                      radius: 28,
-                      backgroundImage: image.image,
-                    ),
-                  ),
-                  SizedBox(width: 14),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        // Name, Category, and Rating in one row
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Expanded(
-                              child: Text(
-                                name,
-                                style: GoogleFonts.libreCaslonText(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.black,
-                                ),
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            ),
-                            SizedBox(width: 8),
-                            const StarDisplayWidget(
-                              filledStar: Icon(Icons.star, color: Colors.yellow, size: 11),
-                              unfilledStar: Icon(Icons.star, color: Colors.grey, size: 11),
+    return Card(
+      color: Colors.white,
+      elevation: 4.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(top: 18, right: 20, bottom: 9),
+              child: StarDisplayWidget(
+                filledStar: Icon(Icons.star, color: Colors.green, size: 11),
+                unfilledStar: Icon(Icons.star, color: Colors.grey, size: 11),
+              ),
+            ),
+            Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20, right: 20, bottom: 10),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey,
+                              blurRadius: 8,
+                              spreadRadius: 2,
+                              offset: Offset.fromDirection(90),
                             ),
                           ],
                         ),
-                        SizedBox(height: 4),
-
-                        Text(
-                          category,
-                          style: GoogleFonts.libreCaslonText(
-                            fontSize: 11,
-                            color: Colors.grey[600],
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(100.0),
+                          child: Image.asset(
+                            'assets/U.png',
+                            fit: BoxFit.cover,
+                            width: 45,
+                            height: 45,
                           ),
                         ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 8),
-              // Description and Hire Button Row
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => Taskpage()));
-
-
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color(0xFF6A3BA8),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 7),
-                    ),
-                    child: Text(
-                      'Hire Now',
-                      style: GoogleFonts.libreCaslonText(
-                        color: Colors.white,
-                        fontSize: 8,
                       ),
                     ),
-                  ),
-                  SizedBox(width: 8),
-
-                  Expanded(
-                    child: Text(
-                      desc,
-                      style: GoogleFonts.libreCaslonText(
-                        fontSize: 12,
-                        color: Colors.grey[800],
-                      ),
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
+                    CustomButton(
+                      fontSize: 7.5,
+                      width: 80,
+                      height: 50,
+                      textcolor: 0xffffffff,
+                      backgroundColor: const Color(0xff6A3BA8),
+                      onPressed: ()=>onPressed(id), text:buttonText,
                     ),
-                  ),
-
-                ],
-              ),
-              SizedBox(height: 12),
-              // Bottom Row with Location, Email, and Phone
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Flexible(
-                    child: Row(
-                      children: [
-                        Icon(Icons.location_on, size: 10, color: Color(0xFF6A3BA8)),
-                        SizedBox(width: 4),
-                        Expanded(
-                          child: Text(
-                            local,
-                            style: GoogleFonts.libreCaslonText(
-                              fontSize: 8,
-                              color: Colors.grey[600],
+                    const SizedBox(height: 22),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          RichText(
+                            text: TextSpan(
+                              children: [
+                                TextSpan(
+                                  text: name,
+                                  style: GoogleFonts.getFont(
+                                    'Libre Caslon Text',
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 13,
+                                    color: const Color(0xff000000),
+                                  ),
+                                ),
+                              ],
                             ),
-                            overflow: TextOverflow.ellipsis,
                           ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Flexible(
-                    child: Row(
-                      children: [
-                        Icon(Icons.email, size: 10, color: Color(0xFF6A3BA8)),
-                        SizedBox(width: 4),
-                        Expanded(
-                          child: Text(
-                            email,
-                            style: GoogleFonts.libreCaslonText(
-                              fontSize: 8,
-                              color: Colors.grey[600],
+                          const SizedBox(width: 18),
+                          RichText(
+                            text: TextSpan(
+                              children: [
+                                TextSpan(
+                                  text: category,
+                                  style: GoogleFonts.getFont(
+                                    'Libre Caslon Text',
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 8,
+                                    color: Color(0xff7F7F7F),
+                                  ),
+                                ),
+                              ],
                             ),
-                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 7),
+                      Container(
+                        width: 200,
+                        child: RichText(
+                          text: TextSpan(
+                            children: [
+                              TextSpan(
+                                text: desc,
+                                style: GoogleFonts.getFont(
+                                  'Libre Caslon Text',
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 10,
+                                  color: const Color(0xff000000),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
-                      ],
-                    ),
-                  ),
-                  Flexible(
-                    child: Row(
-                      children: [
-                        Icon(Icons.phone, size: 10, color: Color(0xFF6A3BA8)),
-                        SizedBox(width: 4),
-                        Text(
-                          number,
-                          style: GoogleFonts.libreCaslonText(
-                            fontSize: 8,
-                            color: Colors.grey[600],
+                      ),
+                      const SizedBox(height: 9),
+                      Row(
+                        children: [
+                          const Icon(
+                            Icons.map,
+                            color: Color(0xff9747FF),
+                            size: 11,
+                            semanticLabel: 'Text to announce in accessibility modes',
                           ),
-                        ),
-                      ],
-                    ),
+                          const SizedBox(width: 5),
+                          Container(
+                            width: 80,
+                            child: RichText(
+                              text: TextSpan(
+                                children: [
+                                  TextSpan(
+                                    text: local,
+                                    style: GoogleFonts.getFont(
+                                      'Libre Caslon Text',
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 7,
+                                      color: const Color(0xff7F7F7F),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          const Column(
+                            children: [
+                              Icon(
+                                Icons.email_rounded,
+                                color: Color(0xff9747FF),
+                                size: 10,
+                                semanticLabel: 'Text to announce in accessibility modes',
+                              ),
+                              SizedBox(height: 2),
+                              Icon(
+                                Icons.phone_android_rounded,
+                                color: Color(0xff9747FF),
+                                size: 10,
+                                semanticLabel: 'Text to announce in accessibility modes',
+                              ),
+                            ],
+                          ),
+                          const SizedBox(width: 2),
+                          Column(
+                            children: [
+                              Container(
+                                width: 90,
+                                child: RichText(
+                                  text: TextSpan(
+                                    children: [
+                                      TextSpan(
+                                        text: email,
+                                        style: GoogleFonts.getFont(
+                                          'Libre Caslon Text',
+                                          fontWeight: FontWeight.w500,
+                                          fontSize: 7,
+                                          color: Color(0xff7F7F7F),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(height: 5),
+                              Container(
+                                width: 90,
+                                child: RichText(
+                                  text: TextSpan(
+                                    children: [
+                                      TextSpan(
+                                        text: number,
+                                        style: GoogleFonts.getFont(
+                                          'Libre Caslon Text',
+                                          fontWeight: FontWeight.w500,
+                                          fontSize: 7,
+                                          color: const Color(0xff7F7F7F),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ],
-          ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
+
   }
 }
-
-
-
-
-
--- lib/View/Widgets/Custome_image_Button.dart
+++ lib/View/Widgets/Custome_image_Button.dart
@@ -8,54 +8,83 @@ class ImageTextButton extends StatelessWidget {
   final double imageHeight;
   final double radius;
   final String text;
-  final Function(dynamic error)? onError; // Error handler
+  final Function(dynamic error)? onError;
 
   const ImageTextButton({
     super.key,
     required this.onPressed,
     required this.image,
-    this.imageHeight = 66,
-    this.radius = 12,
+    this.imageHeight = 77,
+    this.radius = 22,
     required this.text,
-    this.onError, // Optional error handler
+    this.onError,
   });
 
   @override
   Widget build(BuildContext context) {
     return Material(
-      color: const Color(0xff6A3BA8),
-      elevation: 8,
+      color: Colors.transparent, // Remove background color
+      elevation: 0, // Reduced shadow
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(radius)),
-      clipBehavior: Clip.hardEdge,
       child: InkWell(
         onTap: onPressed,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            CachedNetworkImage(
-              imageUrl: image,
-              placeholder: (context, url) => const Center(
-                child: CircularProgressIndicator(),  // عرض تحميل أثناء تحميل الصورة
+        splashColor: Colors.white.withOpacity(0.2),
+        borderRadius: BorderRadius.circular(radius),
+        child: Container(
+          width: 150, // You can adjust the width of the container if needed
+          padding: const EdgeInsets.symmetric(horizontal: 2, vertical: 0), // No vertical padding
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              // Image with reduced size
+              ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(radius - 7),
+                  topRight: Radius.circular(radius - 7),
+                ),
+                child: CachedNetworkImage(
+                  imageUrl: image,
+                  placeholder: (context, url) => const Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                  errorWidget: (context, url, error) {
+                    return Container(
+                      color: Colors.grey[300],
+                      child: const Icon(Icons.broken_image, color: Colors.red),
+                    );
+                  },
+                  width: double.infinity, // Image should take full width
+                  height: imageHeight, // Smaller height for the image
+                  fit: BoxFit.cover,
+                ),
               ),
-              errorWidget: (context, url, error) {
-                return const Icon(Icons.error, color: Colors.white);  // عرض أيقونة خطأ إذا لم تشتغل الصورة
-              },
-              width: 88,
-              height: imageHeight,
-              fit: BoxFit.cover,
-            ),
-            const SizedBox(height: 2),
-            Text(
-              text,
-              style: GoogleFonts.getFont(
-                'Libre Caslon Text',
-                fontWeight: FontWeight.w500,
-                fontSize: 10,
-                color: Colors.white,
+
+              // Purple box with text attached only to the bottom, no space between them
+              Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: const Color(0xff6A3BA8), // Purple background
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(radius - 7),
+                    bottomRight: Radius.circular(radius - 7),
+                  ),
+                ),
+                padding: const EdgeInsets.symmetric(vertical: 4),
+                child: Text(
+                  text,
+                  style: GoogleFonts.getFont(
+                    'Libre Caslon Text',
+                    fontWeight: FontWeight.w500,
+                    fontSize: 12,
+                    color: Colors.white,
+                  ),
+                  textAlign: TextAlign.center,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis, // Avoid overflow
+                ),
               ),
-            ),
-            const SizedBox(height: 5),
-          ],
+            ],
+          ),
         ),
       ),
     );
